02995520280eb131f14af7646316fa375613c313
Nikolay Traykov <traikovn@gmail.com>
2020-05-15 13:46:08 +0200

    Provider onboarding steps layout.

---
+++ src/modules/auth/views/onboarding/BasicInformation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Personal info
+    Personal info
+    Personal info
+    Personal info
+    Personal info
+    Personal info
+    Personal info
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
