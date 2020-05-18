ba600c4d1514d753d5a9c823bb888005bad9655a
Nikolay Traykov <traikovn@gmail.com>
2020-05-18 23:13:54 +0200

    Provider onboarding steps confirmation.

--- src/modules/provider/views/onboarding/Confirmation.vue
+++ src/modules/provider/views/onboarding/Confirmation.vue
@@ -1,15 +1,10 @@
-<template>
-  <div>
-    Personal info
-    Personal info
-    Personal info
-    Personal info
-    Personal info
-    Personal info
-    Personal info
+<template functional>
+  <div class="text-center pl-5 pr-5">
+    <div class="font-weight-bold pt-5 mb-4">
+      Congratulations!
+    </div>
+    <div class="pb-5">
+      Great Job! Your account has now been created. You can update it at anytime in Your Profile. If you sent out any review requests, you will get an email when you have been reviewed.
+    </div>
   </div>
 </template>
-
-<script>
-  export default {}
-</script>
